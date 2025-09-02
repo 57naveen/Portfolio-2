9123bceaecff7b040cc059a45b81b8593d1bda96
57naveen <naveenkishore65@gmail.com>
2025-09-02 17:55:08 +0530

    bug fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./About.css";
 import AboutImg from "../../assets/naveen.jpg";
-import CV from "../../assets/john-Cv.pdf";
+import CV from "../../assets/John-Cv.pdf";
 import Info from "./Info";
 
 const About = () => {
